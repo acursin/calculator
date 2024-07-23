75f40b62739adfd71bc7f1ad24a3815935a9e97d
alex <109973612+acursin@users.noreply.github.com>
2024-07-23 18:46:59 -0400

    Make buttons modify display correctly

--- scripts.js
+++ scripts.js
@@ -21,3 +21,19 @@ function operate (numA, operator, numB) {
             return "ERROR";
     }
 }
+
+const display = document.querySelector('.display');
+
+const numBtns = document.querySelectorAll('.num-btn');
+numBtns.forEach(btn => {
+    btn.addEventListener('click', (event) => {
+        if (display.textContent === '0'){
+            display.textContent = event.target.textContent;
+        } else {
+            display.textContent += event.target.textContent;
+        }
+    })
+});
+
+const clearBtn = document.querySelector('.clear-btn');
+clearBtn.addEventListener('click', () => display.textContent = '0');
