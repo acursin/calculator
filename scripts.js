91c409c56a55e759496f7e159831d8d4563aa324
alex <109973612+acursin@users.noreply.github.com>
2024-07-23 16:36:00 -0400

    Add operate function

--- scripts.js
+++ scripts.js
@@ -1,7 +1,23 @@
 function add(a, b) { return a + b; }
-
 function subtract(a, b) { return a - b; }
-
 function multiply(a, b) { return a * b; }
-
 function divide (a, b) { return a / b; }
+
+let firstNum = '';
+let secondNum = '';
+let curOperator = '';
+
+function operate (numA, operator, numB) {
+    switch (operator) {
+        case '+':
+            return add(numA, numB);
+        case '-':
+            return subtract(numA, numB);
+        case '*':
+            return multiply(numA, numB);
+        case '/':
+            return divide(numA, numB);
+        default:
+            return "ERROR";
+    }
+}
