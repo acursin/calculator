2052a0141607b180ef3e6e20f69310459ad649aa
alex <109973612+acursin@users.noreply.github.com>
2024-07-26 12:12:17 -0400

    Finish keyboard support

--- scripts.js
+++ scripts.js
@@ -31,7 +31,7 @@ let memory = {
 
 const updateDisplay = function (newDisplayNum) {
     if (Number.isNaN(newDisplayNum)) {
-        display.textContent = 'no no no ;)'
+        display.textContent = 'Error!'
     }
     else if (memory.lastBtn !== 'num') {
         display.textContent = newDisplayNum;
@@ -57,84 +57,86 @@ const calculate = function (lastBtnPressed) {
     memory.numToEnter = 'first';
 }
 
-const numPress = function (num) {
-    updateDisplay(num);
+const numPress = function (numInput) {
+    if (numInput === '0' && display.textContent === '0') return;
+    updateDisplay(numInput);
     memory.lastBtn = 'num';
 }
 
-const decimalPress = function (decimal) {
+const decimalPress = function (decimalInput) {
     if (display.textContent.includes('.')) return;
 
     if (memory.lastBtn !== 'num') {
         updateDisplay('0.')
     } else {
-        updateDisplay(decimal);
+        updateDisplay(decimalInput);
     }
     memory.lastBtn = 'num';
 }
 
-const numBtns = document.querySelectorAll('.num-btn');
-numBtns.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        numPress(e.target.textContent);
-    });
-});
-
-const decimalBtn = document.querySelector('.decimal-btn');
-decimalBtn.addEventListener('click', (e) => {
-    decimalPress(e.target.textContent);
-});
-
-document.addEventListener('keydown', (e) => {
-    if (+e.key) {
-        numPress(e.key);
+const operatorPress = function (operatorInput) {
+    if (memory.lastBtn != 'operator' && memory.numToEnter === 'second') {
+        calculate('operator');
     }
+    memory.firstNum = displayNum;
+    memory.operator = operatorInput;
+    memory.numToEnter = 'second';
+    memory.lastBtn = 'operator';
+}
 
-    if (e.key === '.') {
-        decimalPress(e.key);
-    }
-});
-
-
-
-
-
-
-
-const operatorBtns = document.querySelectorAll('.operator-btn');
-operatorBtns.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        if (memory.lastBtn != 'operator' && memory.numToEnter === 'second') {
-            calculate('operator');
-        }
-        memory.firstNum = displayNum;
-        memory.operator = e.target.textContent;
-        memory.numToEnter = 'second';
-        memory.lastBtn = 'operator';
-    });
-});
-
-const equalsBtn = document.querySelector('.equals-btn');
-equalsBtn.addEventListener('click', () => {
+const equalsPress = function() {
     if (memory.operator !== '') calculate('equals');
-});
+}
 
-const backspaceBtn = document.querySelector('.backspace-btn');
-backspaceBtn.addEventListener('click', () => {
+const backspacePress = function() {
     memory.lastBtn = 'backspace';
     if (display.textContent.length === 1) {
         updateDisplay('0');
     } else {
         updateDisplay(display.textContent.slice(0, -1));
     }
-});
+}
 
-const clearBtn = document.querySelector('.clear-btn');
-clearBtn.addEventListener('click', (e) => {
+const clearPress = function() {
     memory.firstNum = 0;
     memory.operator = '';
     memory.secondNum = 0;
     memory.numToEnter = 'first';
     memory.lastBtn = '';
     updateDisplay('0');
+}
+
+const numBtns = document.querySelectorAll('.num-btn');
+numBtns.forEach(btn => {
+    btn.addEventListener('click', (e) => numPress(e.target.textContent));
+});
+
+const decimalBtn = document.querySelector('.decimal-btn');
+decimalBtn.addEventListener('click', (e) => decimalPress(e.target.textContent));
+
+const operatorBtns = document.querySelectorAll('.operator-btn');
+operatorBtns.forEach(btn => {
+    btn.addEventListener('click', (e) => operatorPress(e.target.textContent));
+});
+
+const equalsBtn = document.querySelector('.equals-btn');
+equalsBtn.addEventListener('click', () => equalsPress());
+
+const backspaceBtn = document.querySelector('.backspace-btn');
+backspaceBtn.addEventListener('click', () => backspacePress());
+
+const clearBtn = document.querySelector('.clear-btn');
+clearBtn.addEventListener('click', () => clearPress());
+
+document.addEventListener('keydown', (e) => {
+    if (+e.key || e.key === '0') numPress(e.key);
+
+    else if (e.key === '.') decimalPress(e.key);
+
+    else if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') {
+        operatorPress(e.key);
+    }
+    else if (e.key === '=' || e.key === 'Enter') equalsPress();
+
+    else if (e.key === 'Backspace') backspacePress();
 });
